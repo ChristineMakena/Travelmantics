4d7ead85ab2fd3d39c6231ad93b632465642686e
Makena <xtynmak@GMAIL.COM>
2019-08-08 02:04:35 +0300

    travelmantics fixed image views

---
+++ app/src/main/java/com/example/travelmantics/DealActivity.java
@@ -0,0 +1,180 @@
+package com.example.travelmantics;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+public class DealActivity extends AppCompatActivity {
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
+    ImageView imageView;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    TravelDeal deal;
+    Button btnImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        txtTitle = findViewById(R.id.txtName);
+        txtPrice = findViewById(R.id.txtPrice);
+        txtDescription = findViewById(R.id.txtDescription);
+        imageView = findViewById(R.id.image);
+
+        Intent intent = getIntent();
+        TravelDeal deal = (TravelDeal) intent.getSerializableExtra("Deal");
+        if (deal == null){
+            deal = new TravelDeal();
+        }
+
+        this.deal = deal;
+        txtTitle.setText(deal.getTitle());
+        txtDescription.setText(deal.getDescription());
+        txtPrice.setText(deal.getPrice());
+        showImage(deal.getImageUrl());
+        Log.wtf("SINGLE","***** " + deal.getImageUrl());
+        btnImage = findViewById(R.id.btnImage);
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(Intent.ACTION_GET_CONTENT) ;
+               intent.setType("image/jpeg");
+               intent.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+               startActivityForResult(intent.createChooser(intent,"Insert Picture"),42);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 42 && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            StorageReference ref = FirebaseUtil.mStorageRef.child(imageUri.getLastPathSegment());
+            ref.putFile(imageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    String url = taskSnapshot.getMetadata().getPath();
+                    Log.wtf("PICTURE PATH ","***" + url);
+                    deal.setImageUrl(url);
+                    showImage(url);
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal Saved", Toast.LENGTH_LONG).show();
+                clean();
+                backToList();
+                return true;
+            case R.id.delete_deal:
+                deleteDeal();
+                Toast.makeText(this, "Deal Deleted Successful", Toast.LENGTH_LONG).show();
+                backToList();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void saveDeal(){
+        deal.setTitle(txtTitle.getText().toString());
+        deal.setDescription(txtDescription.getText().toString());
+        deal.setPrice(txtPrice.getText().toString());
+        if(deal.getId() == null){
+            mDatabaseReference.push().setValue(deal);
+        }else{
+            mDatabaseReference.child(deal.getId()).setValue(deal);
+        }
+
+    }
+
+    private void deleteDeal(){
+        if(deal.getId() == null){
+            Toast.makeText(this, "Please Save Deal Before Deleting", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mDatabaseReference.child(deal.getId()).removeValue();
+    }
+
+    private void backToList(){
+        Intent intent = new Intent(this,ListActivity.class);
+        startActivity(intent);
+    }
+
+    private void clean(){
+        txtTitle.setText("");
+        txtDescription.setText("");
+        txtPrice.setText("");
+        txtTitle.requestFocus();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+//        if(FirebaseUtil.isAdmin){
+//            menu.findItem(R.id.delete_deal).setVisible(true);
+//            menu.findItem(R.id.save_menu).setVisible(true);
+//            enableEditText(true);
+//        }else{
+//            menu.findItem(R.id.delete_deal).setVisible(false);
+//            menu.findItem(R.id.save_menu).setVisible(false);
+//            enableEditText(false);
+//        } press ctrl shft f
+
+        return true;
+    }
+//pres ctrl shift F
+    private void showImage(String url){
+        if(url != null && !url.isEmpty()){
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+            url = FirebaseUtil.encodeValue(url); //"https://firebasestorage.googleapis.com/v0/b/travelmantics-56313.appspot.com/o/"+url+"?alt=media&token=011a13ad-ca4d-4b1e-bf02-b3b6cdedd0fa"
+            Picasso.get()
+                    .load("https://firebasestorage.googleapis.com/v0/b/travelmantics-56313.appspot.com/o/"+url+"?alt=media&token=011a13ad-ca4d-4b1e-bf02-b3b6cdedd0fa")
+                    .resize(width,width*2/3)
+                    .placeholder(R.drawable.loading)
+                    .centerCrop()
+                    .into(imageView);
+        }
+    }
+
+    private void enableEditText(boolean isEnabled){
+        txtPrice.setEnabled(isEnabled);
+        txtDescription.setEnabled(isEnabled);
+        txtTitle.setEnabled(isEnabled);
+        btnImage.setEnabled(isEnabled);
+    }
+}
